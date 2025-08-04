82a04e0b4a6050dd8b9bf67ffb85047602567d4c
aram <6aram@naver.com>
2025-08-04 10:38:27 +0900

    buildspec.yml - 코드파이프라인관련

--- src/main/java/com/next/app/api/user/repository/BusinessRepository.java
+++
@@ -1,7 +0,0 @@
-package com.next.app.api.user.repository;
-
-import com.next.app.api.user.entity.Business;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BusinessRepository extends JpaRepository<Business, Long> {
-}
